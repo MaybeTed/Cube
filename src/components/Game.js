6a0563bf813baa77524b30f350a8f5b246bdd71e
Edward White <ewhite91186@gmail.com>
2018-05-18 18:15:49 -0500

    timer starts when start game button is clicked

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Timer from './Timer';
 
 class Game extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			gameStarted: false
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.setState({ gameStarted: !this.state.gameStarted });
+	}
+
 	render() {
 		return (
 			<div>
-				<div className="start-game-button">Start Game</div>
+				{this.state.gameStarted ?
+				<Timer />
+				:
+				<div className="start-game-button" onClick={this.handleClick}>Start Game</div>
+				}
 			</div>
 		)
 	}
---
+++ src/components/Timer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class Timer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sTens: 0,
+			sOnes: 0,
+			mTens: 0,
+			mOnes: 0
+		}
+	}
+
+	componentDidMount() {
+		this.timer = setInterval(this.increaseTime.bind(this), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	}
+
+	increaseTime() {
+		let { sTens, sOnes, mTens, mOnes } = this.state;
+		sOnes++;
+		if (sOnes === 10) {
+			sTens++;
+			sOnes = 0;
+			if (sTens === 6) {
+				mOnes++;
+				sTens = 0;
+				if (mOnes === 10) {
+					mTens++;
+					mOnes = 0;
+				}
+			}
+		}
+		this.setState({ sTens, sOnes, mTens, mOnes });
+	}
+
+	render() {
+		return (
+			<div className="timer-container">
+				<div className="number-container m-tens">{this.state.mTens}</div>
+				<div className="number-container m-ones">{this.state.mOnes}</div>
+				:
+				<div className="number-container s-tens">{this.state.sTens}</div>
+				<div className="number-container s-ones">{this.state.sOnes}</div>
+			</div>
+		)
+	}
+}
+
+export default Timer;
