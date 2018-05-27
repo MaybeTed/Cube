72593dbcf474d8edcb2bc4e14786530385940666
Edward White <ewhite91186@gmail.com>
2018-05-27 18:22:35 -0500

    leaderboard component displays the winners and their times

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import axios from 'axios';
 
 class Leaderboard extends React.Component {
 	constructor () {
 		super();
 		this.state = {
-
+			winners: []
 		}
 	}
 
+	componentDidMount() {
+		axios.get('/api/winners')
+			.then((response) => {
+				this.setState({ winners: response.data.winners });
+			})
+	}
+
 	render() {
+
 		return (
-			<div className="about">
-			  Leaderboard
+			<div className="leaderboard">
+			    <h3>Leaderboard</h3>
+			    <table>
+				  	<tbody>
+					  	<tr>
+					  		<th>Name</th>
+					  		<th>Time</th>
+					  	</tr>
+					  {this.state.winners.map((winner, i) => {
+						  return (
+						  	<tr key={i}>
+						  		<td>{winner.name}</td>
+						  		<td>{winner.time}</td>
+						  	</tr>
+						  )
+					  })
+					  }
+				  	</tbody>
+			    </table>
 			</div>
 		)
 	}
