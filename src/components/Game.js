c19889cb93a16e789d8ba4d80b92f393530d3b10
Edward White <ewhite91186@gmail.com>
2018-05-28 19:22:21 -0500

    added sendWinnerToDatabase function

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import Timer from './Timer';
 
 class Game extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			gameStarted: false
+			gameStarted: false,
+			win: false,
+			winningTime: '',
+			winningSeconds: 0
 		}
 		this.handleClick = this.handleClick.bind(this);
 	}
@@ -13,7 +17,12 @@ class Game extends React.Component {
 	componentWillReceiveProps(nextProps) {
 		if (nextProps.win && this.state.gameStarted === true) {
 			console.log('YOU WIN')
+			this.setState({ win: true });
 		}
+	}
+
+	afterWinning(time, seconds) {
+		this.setState({ winningTime: time, winningSeconds: seconds });
 	}
 
 	handleClick() {
@@ -21,13 +30,41 @@ class Game extends React.Component {
 		this.setState({ gameStarted: !this.state.gameStarted });
 	}
 
+	sendWinnerToDatabase() {
+		event.preventDefault();
+		let name = document.getElementById('winnersName').value;
+		document.getElementById('winnersName').value = '';
+		if (name === null || name === '') {
+			name = 'Anonymous';
+		}
+		axios.post('/api/new-winner', {
+			name,
+			time: this.state.winningTime,
+			seconds: this.state.winningSeconds
+		}).then((response) => {
+			this.setState({ gameStarted: false, win: false, winningTime: '', winningSeconds: 0 });
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				{this.state.gameStarted ?
-				<Timer />
+				<Timer win={this.state.win} afterWinning={this.afterWinning} />
 				:
 				<div className="start-game-button" onClick={this.handleClick}>Start Game</div>
+				}
+				
+				{this.state.winningSeconds > 0 ?
+				<div>
+					<p>Congratulations you won!</p>
+					<form onSubmit={this.sendWinnerToDatabase}>
+						<input placeholder="enter your name" id="winnersName" />
+						<button type="submit">Submit</button>
+					</form>
+				</div>
+				:
+				null
 				}
 			</div>
 		)
